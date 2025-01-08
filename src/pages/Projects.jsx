b7e85b941e66a511e4087c84318311f194e007a5
Emerson Junior <emerson.c.junior.115@gmail.com>
2025-01-08 16:23:18 -0300

    Desenvolvimento do componente Projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,24 +1,89 @@
 import "./Projects.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { projects } from "../data/projects";
 import { useFilterTechnologies } from "../hooks/useFilterTechnologies";
 
 const Projects = () => {
   const [category, setCategory] = useState("Todos");
+  const [activeButton, setActiveButton] = useState(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const changeCategory = (value) => {
+    setIsVisible(false);
+
+    setTimeout(() => {
+      setCategory(value);
+      setIsVisible(true);
+    }, 600);
+  };
 
   return (
-    <section>
-      <h2>Projetos</h2>
-
-      <button onClick={() => setCategory("Todos")}>Todos</button>
-      <button onClick={() => setCategory("Fullstack")}>Fullstack</button>
-      <button onClick={() => setCategory("Landing Page")}>Landing Page</button>
-      <button onClick={() => setCategory("Jogos")}>Jogos</button>
-
-      <div id="project-cards-container">
+    <section id="projects">
+      <div id="projects-title-container">
+        <h1 id="projects-title">Projetos</h1>
+      </div>
+      <div id="category-buttons-container">
+        <button
+          className={
+            activeButton === "1"
+              ? "button-active category-button"
+              : "category-button"
+          }
+          onClick={() => {
+            changeCategory("Todos");
+            setActiveButton("1");
+          }}
+        >
+          Todos
+        </button>
+        <button
+          className={
+            activeButton === "2"
+              ? "button-active category-button"
+              : "category-button"
+          }
+          onClick={() => {
+            changeCategory("Fullstack");
+            setActiveButton("2");
+          }}
+        >
+          Fullstack
+        </button>
+        <button
+          className={
+            activeButton === "3"
+              ? "button-active category-button"
+              : "category-button"
+          }
+          onClick={() => {
+            changeCategory("Landing Page");
+            setActiveButton("3");
+          }}
+        >
+          Landing Page
+        </button>
+        <button
+          className={
+            activeButton === "4"
+              ? "button-active category-button"
+              : "category-button"
+          }
+          onClick={() => {
+            changeCategory("Jogos");
+            setActiveButton("4");
+          }}
+        >
+          Jogos
+        </button>
+      </div>
+      <div
+        className={`project-cards-container ${
+          isVisible ? "active-animation" : ""
+        }`}
+      >
         {projects.map((project, index) => {
           const arrCategories = useFilterTechnologies(project.technologies);
-          if(!project.categories.includes(category)){
+          if (!project.categories.includes(category)) {
             return;
           }
           return (
@@ -31,33 +96,39 @@ const Projects = () => {
                 <button className="project-button">Acessar</button>
               </div>
               <div className="project-info-container">
-                <div className="project-title-wrapper">
-                  <h3>{project.name}</h3>
+                <div className="card-top-container">
+                  <div className="project-title-wrapper">
+                    <h3>{project.name}</h3>
+                  </div>
+                  <div className="acess-icons-container">
+                    <i className="acess-icon fa-brands fa-github"></i>
+                    <i className="acess-icon fa-solid fa-up-right-from-square"></i>
+                  </div>
                 </div>
                 <div className="project-description">{project.description}</div>
               </div>
               <div className="line"></div>
               <div className="project-icons">
                 {arrCategories.includes("1") && (
-                  <i className="icon devicon-html5-plain"></i>
+                  <i className="devicon-html5-plain"></i>
                 )}
                 {arrCategories.includes("2") && (
-                  <i className="icon devicon-css3-plain"></i>
+                  <i className="devicon-css3-plain"></i>
                 )}
                 {arrCategories.includes("3") && (
-                  <i className="icon devicon-javascript-plain"></i>
+                  <i className="devicon-javascript-plain"></i>
                 )}
                 {arrCategories.includes("4") && (
-                  <i className="icon devicon-csharp-plain"></i>
+                  <i className="devicon-csharp-plain"></i>
                 )}
                 {arrCategories.includes("5") && (
-                  <i className="icon devicon-dotnetcore-plain"></i>
+                  <i className="devicon-dotnetcore-plain"></i>
                 )}
                 {arrCategories.includes("6") && (
-                  <i className="icon devicon-react-original"></i>
+                  <i className="devicon-react-original"></i>
                 )}
                 {arrCategories.includes("7") && (
-                  <i className="icon devicon-firebase-plain"></i>
+                  <i className="devicon-firebase-plain"></i>
                 )}
               </div>
             </div>
