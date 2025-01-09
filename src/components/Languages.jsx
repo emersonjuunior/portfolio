4720fc5a304b34c0a446338a86ca8312a90f6a6e
Emerson Junior <emerson.c.junior.115@gmail.com>
2025-01-09 20:37:23 -0300

    Renderização dinâmica dos cards de linguagens

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -1,107 +1,22 @@
 import "./Languages.css";
+import { languages } from "../data/languages";
 
 const Languages = () => {
   return (
     <section id="languages">
       <h2>Tecnologias</h2>
       <div id="languages-container">
-        <div className="languages-card">
-          <div className="bg"></div>
-          <div className="blob"></div>
-          <div className="tech-info">
-            <div className="icon-wrapper">
-              <i className="devicon-html5-plain"></i>
-            </div>
-            <div className="tech-wrapper">
-              <p className="tech-name">HTML</p>
-            </div>
+        {languages.map((language) => (
+          <div className="languages-card">
+            <div className="bg"></div>
+            <div className="blob"></div>
+
+            <i className={language.icon}></i>
+            <p className="tech-name">{language.name}</p>
+
+            <div className="hidden">{language.name}</div>
           </div>
-          <div className="hidden">HTML</div>
-        </div>
-        <div className="languages-card">
-          <div className="bg"></div>
-          <div className="blob"></div>
-          <div className="tech-info">
-            <div className="icon-wrapper">
-              <i className="devicon-css3-plain"></i>
-            </div>
-            <div className="tech-wrapper">
-              <p className="tech-name"></p>
-            </div>
-          </div>
-        </div>
-        <div className="languages-card">
-          <div className="bg"></div>
-          <div className="blob"></div>
-          <div className="tech-info">
-            <div className="icon-wrapper">
-              <i className="devicon-javascript-plain"></i>
-            </div>
-            <div className="tech-wrapper">
-              <p className="tech-name"></p>
-            </div>
-          </div>
-        </div>
-        <div className="languages-card">
-          <div className="bg"></div>
-          <div className="blob"></div>
-          <div className="tech-info">
-            <div className="icon-wrapper">
-              <i className="devicon-react-original"></i>
-            </div>
-            <div className="tech-wrapper">
-              <p className="tech-name"></p>
-            </div>
-          </div>
-        </div>
-        <div className="languages-card">
-          <div className="bg"></div>
-          <div className="blob"></div>
-          <div className="tech-info">
-            <div className="icon-wrapper">
-              <i className="devicon-sass-plain"></i>
-            </div>
-            <div className="tech-wrapper">
-              <p className="tech-name"></p>
-            </div>
-          </div>
-        </div>
-        <div className="languages-card">
-          <div className="bg"></div>
-          <div className="blob"></div>
-          <div className="tech-info">
-            <div className="icon-wrapper">
-              <i className="devicon-firebase-plain"></i>
-            </div>
-            <div className="tech-wrapper">
-              <p className="tech-name"></p>
-            </div>
-          </div>
-        </div>
-        <div className="languages-card">
-          <div className="bg"></div>
-          <div className="blob"></div>
-          <div className="tech-info">
-            <div className="icon-wrapper">
-              <i className="devicon-git-plain"></i>
-            </div>
-            <div className="tech-wrapper">
-              <p className="tech-name"></p>
-            </div>
-          </div>
-        </div>
-        <div className="languages-card">
-          <div className="bg"></div>
-          <div className="blob"></div>
-          <div className="tech-info">
-            <div className="icon-wrapper">
-              <i className="fa-brands fa-github"></i>
-            </div>
-            <div className="tech-wrapper">
-              <p className="tech-name"></p>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
--- src/data/languages.js
+++ src/data/languages.js
@@ -1,1 +1,33 @@
-export const languages = {}
+export const languages = [{
+    icon: "devicon-html5-plain",
+    name: "HTML"
+},
+{
+    icon: "devicon-css3-plain",
+    name: "CSS"
+},
+{
+    icon: "devicon-javascript-plain",
+    name: "JavaScript"
+},
+{
+    icon: "devicon-react-original",
+    name: "React"
+},
+{
+    icon: "devicon-sass-plain",
+    name: "Sass"
+},
+{
+    icon: "devicon-firebase-plain",
+    name: "FireBase"
+},
+{
+    icon: "devicon-git-plain",
+    name: "Git"
+},
+{
+    icon: "devicon-github-plain",
+    name: "Github"
+},
+]
