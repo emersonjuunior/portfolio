98a863c8a2dbade46f1a8974e75f27b43ead6f12
Emerson Junior <emerson.c.junior.115@gmail.com>
2025-01-09 20:07:09 -0300

    Componente de Linguagens

---
+++ src/components/Languages.jsx
@@ -0,0 +1,110 @@
+import "./Languages.css";
+
+const Languages = () => {
+  return (
+    <section id="languages">
+      <h2>Tecnologias</h2>
+      <div id="languages-container">
+        <div className="languages-card">
+          <div className="bg"></div>
+          <div className="blob"></div>
+          <div className="tech-info">
+            <div className="icon-wrapper">
+              <i className="devicon-html5-plain"></i>
+            </div>
+            <div className="tech-wrapper">
+              <p className="tech-name">HTML</p>
+            </div>
+          </div>
+          <div className="hidden">HTML</div>
+        </div>
+        <div className="languages-card">
+          <div className="bg"></div>
+          <div className="blob"></div>
+          <div className="tech-info">
+            <div className="icon-wrapper">
+              <i className="devicon-css3-plain"></i>
+            </div>
+            <div className="tech-wrapper">
+              <p className="tech-name"></p>
+            </div>
+          </div>
+        </div>
+        <div className="languages-card">
+          <div className="bg"></div>
+          <div className="blob"></div>
+          <div className="tech-info">
+            <div className="icon-wrapper">
+              <i className="devicon-javascript-plain"></i>
+            </div>
+            <div className="tech-wrapper">
+              <p className="tech-name"></p>
+            </div>
+          </div>
+        </div>
+        <div className="languages-card">
+          <div className="bg"></div>
+          <div className="blob"></div>
+          <div className="tech-info">
+            <div className="icon-wrapper">
+              <i className="devicon-react-original"></i>
+            </div>
+            <div className="tech-wrapper">
+              <p className="tech-name"></p>
+            </div>
+          </div>
+        </div>
+        <div className="languages-card">
+          <div className="bg"></div>
+          <div className="blob"></div>
+          <div className="tech-info">
+            <div className="icon-wrapper">
+              <i className="devicon-sass-plain"></i>
+            </div>
+            <div className="tech-wrapper">
+              <p className="tech-name"></p>
+            </div>
+          </div>
+        </div>
+        <div className="languages-card">
+          <div className="bg"></div>
+          <div className="blob"></div>
+          <div className="tech-info">
+            <div className="icon-wrapper">
+              <i className="devicon-firebase-plain"></i>
+            </div>
+            <div className="tech-wrapper">
+              <p className="tech-name"></p>
+            </div>
+          </div>
+        </div>
+        <div className="languages-card">
+          <div className="bg"></div>
+          <div className="blob"></div>
+          <div className="tech-info">
+            <div className="icon-wrapper">
+              <i className="devicon-git-plain"></i>
+            </div>
+            <div className="tech-wrapper">
+              <p className="tech-name"></p>
+            </div>
+          </div>
+        </div>
+        <div className="languages-card">
+          <div className="bg"></div>
+          <div className="blob"></div>
+          <div className="tech-info">
+            <div className="icon-wrapper">
+              <i className="fa-brands fa-github"></i>
+            </div>
+            <div className="tech-wrapper">
+              <p className="tech-name"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Languages;
--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,16 +1,17 @@
-import "./Skills.css"
-import Slider from "../../components/Slider"
+import "./Skills.css";
+import Languages from "../../components/Languages";
+import Slider from "../../components/Slider";
 
 const Skills = () => {
-    
   return (
     <section>
       <div className="section-title">
         <h1 className="section-h1">Habilidades</h1>
       </div>
-      <Slider/>
+      <Languages />
+      <Slider />
     </section>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
