d2fc0e7a83ad3937f312ee2cb5f2ced4234cc02d
Emerson Junior <emerson.c.junior.115@gmail.com>
2025-02-19 16:29:32 -0300

    Botão de baixar o currículo

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
     <section id="about">
       <div id="about-container">
         <div id="me-picture">
-          <img src="/profile.jpg" alt="" />
+          <img src="/profile.jpg" alt="Emerson Junior" />
           <div className="border-img" id="border-1"></div>
           <div className="border-img" id="border-2"></div>
         </div>
@@ -21,18 +21,18 @@ const About = () => {
         <div id="about-text">
           <p className="about-p">
             <span className="first-letter">M</span>eu interesse por tecnologia
-            sempre existiu, desde quando passava horas refletindo
-            sobre o que havia por trás dos softwares presentes no meu dia a dia.
-            A ideia de buscar soluções inteligentes e inovadoras para problemas
-            já existentes continua me motivando até hoje.
+            sempre existiu, desde quando passava horas refletindo sobre o que
+            havia por trás dos softwares presentes no meu dia a dia. A ideia de
+            buscar soluções inteligentes e inovadoras para problemas já
+            existentes continua me motivando até hoje.
           </p>
           <p className="about-p">
             <span className="first-letter">A</span>tualmente, estou cursando
             Análise e Desenvolvimento de Sistemas na PUC Minas. Meu foco está no
             desenvolvimento front-end, criando componentes reutilizáveis e
             interfaces intuitivas e responsivas. Também possuo experiência em
-            integração de APIs, metodologias ágeis (SCRUM), além de
-            já ter desenvolvido no back-end com tecnologias como .NET e C#.
+            integração de APIs, metodologias ágeis (SCRUM), além de já ter
+            desenvolvido no back-end com tecnologias como .NET e C#.
           </p>
           <p className="about-p" id="p-3">
             <span className="first-letter">M</span>eu objetivo é estar sempre em
@@ -41,10 +41,15 @@ const About = () => {
             qualidade e para uma melhor experiência do usuário.
           </p>
           <div id="about-btn-container">
-            <button className="primary-button" id="about-button">
-              <i className="download fa-solid fa-file-arrow-down"></i> Baixar
-              Resumo
-            </button>
+            <a
+              href="/Currículo_Desenvolvedor_Emerson_Junior.pdf"
+              download="/Currículo_Desenvolvedor_Emerson_Junior.pdf"
+            >
+              <button className="primary-button" id="about-button">
+                <i className="download fa-solid fa-file-arrow-down"></i> Baixar
+                Resumo
+              </button>
+            </a>
             <NextButton name={"Habilidades"} link={"/skills"} />
           </div>
         </div>
